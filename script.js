c3ce2041497187b6768ac621cfd99c003694a681
SAGfotografia <ph.sofiacosta@gmail.com>
2025-01-30 09:31:11 -0300

    Primer commit - Subida inicial

---
+++ script.js
@@ -0,0 +1,57 @@
+// Scroll animado al hacer clic en los enlaces del menú
+document.querySelectorAll('.seccion').forEach(enlace => {
+    enlace.addEventListener('click', function(e) {
+        e.preventDefault();
+        const destino = document.querySelector(this.getAttribute('href'));
+        destino.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// VISOR DE IMÁGENES
+let imagenesEvento = [
+    ["images/1/1.jpg", "images/1/3.jpg", "images/1/4.jpg", "images/1/5.jpg", "images/1/2.jpg", "images/1/6.jpg", "images/1/7.jpg", "images/1/8.jpg", "images/1/9.jpg", "images/1/10.jpg"],
+    ["images/2/1.jpg", "images/2/2.jpg", "images/2/3.jpg", "images/2/4.jpg", "images/2/5.jpg", "images/2/6.jpg", "images/2/7.jpg", "images/2/8.jpg", "images/2/9.jpg", "images/2/10.jpg"],
+    ["images/3/1.jpg", "images/3/2.jpg", "images/3/3.jpg", "images/3/4.jpg", "images/3/5.jpg", "images/3/6.jpg", "images/3/7.jpg", "images/3/8.jpg"],
+    ["images/4/1.jpg", "images/4/2.jpg", "images/4/3.jpg", "images/4/4.jpg", "images/4/5.jpg", "images/4/6.jpg", "images/4/7.jpg", "images/4/8.jpg"],
+    ["images/5/1.jpg", "images/5/2.jpg", "images/5/3.jpg", "images/5/4.jpg", "images/5/6.jpg", "images/5/7.jpg"],
+    ["images/6/1.jpg", "images/6/3.jpg", "images/6/5.jpg", "images/6/6.jpg", "images/6/7.jpg", "images/6/8.jpg", "images/6/9.jpg", "images/6/10.jpg", "images/6/2.jpg", "images/6/4.jpg"]
+];
+
+let indiceActual = 0;
+let eventoActual = 0;
+
+function abrirVisor(indice) {
+    indiceActual = indice;
+    document.getElementById("visorImg").src = imagenesEvento[eventoActual][indice];
+    document.getElementById("visor").style.display = "flex";
+}
+
+function cerrarVisor() {
+    document.getElementById("visor").style.display = "none";
+}
+
+function cambiarImagen(direccion) {
+    indiceActual += direccion;
+    if (indiceActual < 0) {
+        indiceActual = imagenesEvento[eventoActual].length - 1;
+    } else if (indiceActual >= imagenesEvento[eventoActual].length) {
+        indiceActual = 0;
+    }
+    document.getElementById("visorImg").src = imagenesEvento[eventoActual][indiceActual];
+}
+
+// CAMBIO DE EVENTOS EN EL PORTAFOLIO
+function cambiarEvento(evento) {
+    eventoActual = evento;
+    let gallery = document.querySelector('.gallery');
+    gallery.innerHTML = "";
+    
+    imagenesEvento[eventoActual].forEach((imgSrc, index) => {
+        let img = document.createElement("img");
+        img.src = imgSrc;
+        img.alt = "Fotografía " + (index + 1);
+        img.onclick = () => abrirVisor(index);
+        gallery.appendChild(img);
+    });
+}
+
